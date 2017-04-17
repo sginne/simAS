5a3ef85bf453c338aab178b99d8a971645612500
sginne <sigin@norsula.com>
2017-04-17 10:52:17 +0300

    Network class is moved to separate container

--- database.py
+++ database.py
@@ -4,49 +4,13 @@ Created on Apr 2, 2017
 @author: sginne
 '''
 import sqlite3
+from router import Router
+from port import Port
+from wire import Wire
 DbConnection = None
 DbCursor= None
 
-class Network(object):
-    Routers=[]
-    def __init__(self):
-        for RouterObject in ListRouters():
-            self.Routers.append(self.Router(RouterObject.Id,RouterObject.Name))
-                    
-    class Router(object):
-        Id=0
-        Name='x'
-        Ports=[]
-        def __init__(self,Id,Name):
-            self.Id=Id
-            self.Name=Name
-            self.Ports=[]
-            for PortObject in ListPorts():
-                #print(PortObject.RouterId,self.Name)
-                if (PortObject.RouterId==self.Id):
-                    #self.Ports.append('a')                   
-                    self.Ports.append(self.Port(PortObject.Id,PortObject.RouterId,PortObject.Ip))
-        class Port(object):
-            Id=0
-            RouterId=0
-            Ip='x.x.x.x'
-            def __init__(self,Id,RouterId,Ip):
-                self.Id=Id
-                self.RouterId=RouterId
-                self.Ip=Ip
-            
 
-class Router(object):
-    Id=0
-    Name='X'
-class Port(object):
-    Id=0
-    RouterId=0
-    Ip='x.x.x.x'
-class Wire(object):
-    Id=0
-    Port1Id=0
-    Port2Id=0
     
 #database initialization and sql command execution.
 def InitSQL(FileName): 
@@ -68,7 +32,7 @@ def ListPorts(Where=''):
     DbCursor=DbConnection.cursor()
     #cursor=DbCursor.execute("SELECT * FROM PORTS "+Where)
     for row in DbCursor.execute("SELECT * FROM PORTS "+Where).fetchall():
-        Obj=Router()
+        Obj=Port()
         Obj.Id=row[0]
         Obj.RouterId=row[1]
         Obj.Ip=row[2]
